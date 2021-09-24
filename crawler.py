359d155ebd04f4e883a6d99c2346115f7a4b469b
Glykeria99 <glykfoun@csd.auth.gr>
2021-09-24 12:07:38 +0300

    Added threads in crawler + small changes

--- crawler.py
+++ crawler.py
@@ -1,48 +1,82 @@
-import os
-import urllib
 import requests
 from bs4 import BeautifulSoup
-import base64
+from urllib.parse import urljoin
+import threading
 
-class Crawler:
-    def myCrawler(url, max_pages, save, threads):
-        if save == 0:  # if the user requested to restart the crawler and delete all the previous data
-            for f in os.listdir(".\\files\\"):  # delete all the txt files
-                if not f.endswith(".txt"):
-                    continue
-                os.remove(os.path.join(".\\files\\", f))
 
-        page_num = 0  # number of pages we have visited
-        urls = [url]  # urls saves all the urls found
-        visited = [url]   # visited contains all the already visited urls, so we dont have to visit them again
-        while page_num < max_pages:
-            res = requests.get(urls[page_num])
-            html_page = res.content
-            soup = BeautifulSoup(html_page, 'html.parser')  # create a new bs4 object from the html data loaded
-            soup.encode("utf-8")
-            for script in soup(["script"]):  # extract only the text from the html file
-                script.extract()
-            text = soup.get_text()
+class Crawler(threading.Thread):
 
-            # finds other urls in the current page
-            for link in soup.findAll('a', href=True): # finds the contents of the tags <a> that have an url
-                link['href'] = urllib.parse.urljoin(urls[0], link['href'])
-                if link['href'] not in visited:  # if the link is not in visited appends it to urls and visited
-                    if '.pdf' not in link['href'] and '.jpg' not in link['href']:  # makes sure no jpg or pdfs pass
-                        urls.append(link['href'])
-                        visited.append(link['href'])
+    def __init__(self, url, links_to_crawl, visited, max_pages, save, url_lock):
+        threading.Thread.__init__(self)
+        print(f"Web Crawler worker {threading.current_thread()} has Started")
+        self.max_pages = max_pages
+        self.save = save
+        self.url = url
+        self.links_to_crawl = links_to_crawl
+        self.visited = visited
+        self.url_lock = url_lock
 
-            title = urls[page_num]  # get the title of the current page.
-            title = title.replace("\n", "")  # removing all the unnecessary things from the title
-            title = title.replace("\r", "")
-            title = title.replace("\t", "")
-            title = title.replace("|", "")
-            title = title.replace(":", "")
-            title = title.replace("/", "")
-            title = title.strip(' ')
+    def run(self):
 
-            # creates a txt file with the name of the page
-            file = open(".\\files\\" + title + ".txt", "w", encoding='utf-8')
-            file.write(text)                                 # and saves the text.
-            page_num = page_num + 1  # increase the number of pages we have visited.
+        while True:
+            # In this part of the code we create a global lock on our queue of
+            # links so that no two threads can access the queue at same time
+            self.url_lock.acquire()
+            link = self.links_to_crawl.get()
+            self.url_lock.release()
+
+            # if the link is None the queue is exhausted or the threads are yet
+            # process the links.
+
+            if link is None:
+                break
+
+            # if The link is already visited we break the execution.
+            if link in self.visited:
+                break
+
+            try:
+                # This method constructs a full "absolute" URL by combining the
+                # base url with other url. this uses components of the base URL,
+                # in particular the addressing scheme, the network
+                # location and  the path, to provide missing components
+                # in the relative URL.
+                # in short we repair our relative url if it is broken.
+                link = urljoin(self.url, link)
+
+                res = requests.get(link)
+                html_page = res.content
+                if len(self.visited) >= self.max_pages:
+                    break
+                # this returns the html representation of the webpage
+                soup = BeautifulSoup(html_page, "html.parser")
+
+                # in this case we are finding all the links in the page.
+                for a_tag in soup.find_all('a'):
+                    # we are checking of the link is already visited and (network location part) is our
+                    # base url itself.
+                    if (a_tag.get("href") not in self.visited):
+                        self.links_to_crawl.put(a_tag.get("href"))
+
+
+                print(f"Adding {link} to the crawled list")
+                self.visited.add(link)
+                text = soup.get_text()
+
+                title = link  # get the title of the current page.
+                title = title.replace("\n", "")  # removing all the unnecessary things from the title
+                title = title.replace("\r", "")
+                title = title.replace("\t", "")
+                title = title.replace("|", "")
+                title = title.replace(":", "")
+                title = title.replace("/", "")
+                title = title.strip(' ')
+                print(title)
+                # creates a txt file with the name of the page
+                file = open(".\\files\\" + title + ".txt", "w", encoding='utf-8')
+                file.write(text)  # and saves the text.
+                if len(self.visited) > self.max_pages:
+                    break
+            finally:
+                self.links_to_crawl.task_done()
 
--- main.py
+++ main.py
@@ -1,19 +1,40 @@
 import indexer
 import crawler
+import queue
+import threading
+import os
 import queryprocessor
 
 # os.mkdir(".\\files")
-url = "auth.gr"  # url
-pages = 5  # number of pages to iterate
+url = "https://www.auth.gr/" # url
+links_to_crawl = queue.Queue()
+url_lock = threading.Lock()
+links_to_crawl.put(url)
+crawler_threads = []
+
+
+pages = int(input("Give the number of pages to crawl:"))  # number of pages to iterate
 save = 0  # keep last data or delete it (1 = keep, 0 = delete)
-threads = 8  # number of threads (not using it)
+num_of_threads = int(input("Give the number of threads:"))# number of threads
+visited = set()
+if save == 0:  # if the user requested to restart the crawler and delete all the previous data
+    for f in os.listdir(".\\files\\"):  # delete all the txt files
+        if not f.endswith(".txt"):
+            continue
+        os.remove(os.path.join(".\\files\\", f))
 if not url.startswith("http"):
     url = "http://" + url
-MyCrawler = crawler.Crawler
-MyCrawler.myCrawler(url, pages, save, threads)
+for i in range(int(num_of_threads)):
+    MyCrawler = crawler.Crawler(url, links_to_crawl, visited, pages, save, url_lock)
+    MyCrawler.start()
+    crawler_threads.append(MyCrawler)
+for crawler in crawler_threads:
+    crawler.join()
+
+
+print(f"Total Number of pages visited are {len(visited)} using {num_of_threads} threads")
 
 df_count, num_of_words_in_docs, indexer_copy = indexer.myInvertedIndexer()
 query = queryprocessor.queryProcessor()
 query.calculate_cosine_sim("Greek university", pages, df_count, num_of_words_in_docs, indexer_copy)
-
 
