d8545b6e9847e9f3d45f6ebcb8e35d9bd7d502fe
katerinach <33007374+katerinach@users.noreply.github.com>
2021-09-28 19:52:43 +0300

    correction to csv writer

--- indexer.py
+++ indexer.py
@@ -10,48 +10,48 @@ def Indexer():
     num_of_words_in_doc = [] #tracks the total number of words in each document
 
     #getting each word from each file and creating a csv file to save the data
-    with open('.\\indexer\\indexer.csv', 'w', newline='', encoding='utf-8') as csv_file:
 
-        for file in os.listdir(".\\files\\"):  # for each txt file created by crawler
-            words_count = 0
-            if not file.endswith(".txt"):
-                continue
-                #reading each line
-            with open(".\\files\\" + file, 'r',  encoding='utf-8') as f:
-                for line in f:
-                    #reading each word
-                    for word in line.split():
-                        punc = '''!()|-[]{};:'", <>./?@#$%^&*_~©®™¨¨«»'''
-                        if word in punc: #ignore punctuation marks
-                            continue
-                        words_count = words_count+1
-                        flag = 0
-                        for index, array in enumerate(list):
-                            if word in array[0] and str(file) == str(array[1]):
-                                list[index][2] = int(list[index][2]) + 1
-                                flag = 1
-                        if flag == 0:
-                            word = word.lower() #make all words lowercase to avoid same words appearing in the indexer multiple times
-                            list.append(numpy.array([str(word), str(file), 1])) #add the word,document name to the list
-                        else:
-                            continue
-            num_of_words_in_doc.append([file, words_count]) #add the file and word count
-        #sorting the csv in alphabetic order by the 'word' value
-        list.sort(key=get_word)
 
-        #calculating the number of documents that each word appears in (so that the same word doesn't repeat)
-        count = []
-        frequency = 0
-        previous_word = list[0][0] #set the first word as the previous word
-        for array in list: #for each word in the list
-            current_word = array[0] #set each word as current
-            if current_word == previous_word: #if it's the same as the previous one
-                frequency = frequency + 1 #add frequency count
-                previous_word = current_word #set current as previous
-            else:
-                count.append([previous_word, frequency]) #else means the repeated word has ended and we can add it and frequency count to the list
-                frequency = 1 #resetting frequency count
-                previous_word = current_word #set the current word as previous
+    for file in os.listdir(".\\files\\"):  # for each txt file created by crawler
+        words_count = 0
+        if not file.endswith(".txt"):
+            continue
+            #reading each line
+        with open(".\\files\\" + file, 'r',  encoding='utf-8') as f:
+            for line in f:
+                #reading each word
+                for word in line.split():
+                    punc = '''!()|-[]{};:'", <>./?@#$%^&*_~©®™¨¨«»'''
+                    if word in punc: #ignore punctuation marks
+                        continue
+                    words_count = words_count+1
+                    flag = 0
+                    for index, array in enumerate(list):
+                        if word in array[0] and str(file) == str(array[1]): #for same words in the same document
+                            list[index][2] = int(list[index][2]) + 1
+                            flag = 1
+                    if flag == 0:
+                        word = word.lower() #make all words lowercase to avoid same words appearing in the indexer multiple times
+                        list.append(numpy.array([str(word), str(file), 1])) #add the word,document name to the list
+                    else:
+                        continue
+        num_of_words_in_doc.append([file, words_count]) #add the file and word count
+    #sorting the csv in alphabetic order by the 'word' value
+    list.sort(key=get_word)
+
+    #calculating the number of documents that each word appears in (so that the same word doesn't repeat)
+    count = []
+    frequency = 0
+    previous_word = list[0][0] #set the first word as the previous word
+    for array in list: #for each word in the list
+        current_word = array[0] #set each word as current
+        if current_word == previous_word: #if it's the same as the previous one
+            frequency = frequency + 1 #add frequency count
+        else:
+            count.append([previous_word, frequency]) #else means the repeated word has ended and we can add it and frequency count to the list
+            frequency = 1 #resetting frequency count
+
+        previous_word = current_word #set current as previous
 
     #creating a csv file to save the data
     indexer_copy = []
